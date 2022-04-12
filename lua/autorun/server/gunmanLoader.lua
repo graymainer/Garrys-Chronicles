a7056f3a66e807c53bb5ae3f26b4e5fa1f4f2289
graymainer <50006274+graymainer@users.noreply.github.com>
2022-04-12 05:13:31 -0400

    finalized city 1 & 2, needs built.
    resolved issue #39 .
    resolved issue #44 .
    resolved issue #43 .
    resolved issue #42 .
    resolved issue #41 .
    tram train can no longer be deleted with context menu.
    resolved issue #40 .
    tram monitors now have 'toggle' solidity value.
    redid tram sound library.
    now have sound library dedicated for future gunman soundscripts.
    renamed sentences_gunman to gunmanSentences to remain naming consistency.
    resolved issue #38 , now city1 will fire "sound.Play('Tram.Voice0', Vector(0, 0, 0))" to play the voice over. This is done because ambient_generics do not support specifying sound channels.
    got rid of invalid trigger_once with no model in city1.
    fixed player spawning too high in city1.
    resized spawn trigger in city1.
    tidied up out of bounds areas in city2.
    start_plat now lowers when player is on it in city2.
    renamed city1 & 2 vmfs to match. fixed soundscapes.
    puzzleman no longer checks for gc_city03_22_2
    compiled final builds for gunman_city2 and gunman_city1

--- lua/autorun/server/gunmanLoader.lua
+++ lua/autorun/server/gunmanLoader.lua
@@ -6,39 +6,11 @@
 if ( !string.StartWith(game.GetMap(), "gc_") and !string.StartWith(game.GetMap(), "gunman_")) then return end
 
 
-PrecacheSentenceFile("data/gunman/sentences_gunman.json")
+PrecacheSentenceFile("data/gunman/gunmanSentences.json")
 
---loads some soundscript entries for npc footsteps.
 
-sound.Add( {
-	name = "NPC.Gunman_Step1",
-	channel = CHAN_BODY,
-	volume = 15,
-	level = SNDLVL_75dB, --must be set (not equal to 0) for spatialization to work. not sure what to, though...
-	pitch = {95, 105},
-	sound = 
-		{
-			"gunman/common/gunman_step1.wav",
-			"gunman/common/gunman_step3.wav"
-		}
-	}
-)
-
-sound.Add( {
-	name = "NPC.Gunman_Step2",
-	channel = CHAN_BODY,
-	volume = 15,
-	level = SNDLVL_75dB,
-	pitch = {95, 105},
-	sound = 
-		{
-			"gunman/common/gunman_step2.wav",
-			"gunman/common/gunman_step4.wav"
-		}
-	}
-)
+include("gunman/gunmanSoundLib.lua")
 
 include("gunman/gunmanPuzzleMan.lua")
---include("gunman/soundLib.lua")
 
 include("gunman/gunmanMapLoader.lua")
---
+++ lua/gunman/gunmanSoundLib.lua
@@ -0,0 +1,62 @@
+--loads up any soundscripts we need for the project.
+
+
+--entries for the vo system in the tram ride section.
+sound.Add( {
+	name = "Tram.Voice0",
+	channel = CHAN_VOICE,
+	volume = 0.6,
+	level = 0,
+	pitch = 100,
+	sound = "gunman/tram/tramVO0.wav"
+	}
+)
+
+sound.Add( {
+	name = "Tram.Voice1",
+	channel = CHAN_VOICE,
+	volume = 0.6,
+	level = 0,
+	pitch = 100,
+	sound = "gunman/tram/tramVO1.wav"
+	}
+)
+
+--tram vo players
+function playTramVO0() --needed for some dumbass reason im not sure anyone will ever understand. this same exact code cant just be executed in hammer for who knows what reason. even with the "s replaced with the 's. nope.
+	sound.Play("Tram.Voice0", Vector(0, 0, 0))
+end
+
+function playTramVO1()
+	sound.Play("Tram.Voice1", Vector(0, 0, 0))
+end
+
+--entries for npc footsteps.
+
+sound.Add( {
+	name = "NPC.Gunman_Step1",
+	channel = CHAN_BODY,
+	volume = 15,
+	level = SNDLVL_75dB, --must be set (not equal to 0) for spatialization to work. not sure what to, though...
+	pitch = {95, 105},
+	sound = 
+		{
+			"gunman/common/gunman_step1.wav",
+			"gunman/common/gunman_step3.wav"
+		}
+	}
+)
+
+sound.Add( {
+	name = "NPC.Gunman_Step2",
+	channel = CHAN_BODY,
+	volume = 15,
+	level = SNDLVL_75dB,
+	pitch = {95, 105},
+	sound = 
+		{
+			"gunman/common/gunman_step2.wav",
+			"gunman/common/gunman_step4.wav"
+		}
+	}
+)
