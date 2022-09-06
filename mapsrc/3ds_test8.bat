bb30ecd6e94f1b8717ae8b0bf81400ea67fe141c
graymainer <50006274+graymainer@users.noreply.github.com>
2022-09-06 01:20:56 -0400

    redid arena roof
    --CITY4--
    
    [ARENA]
    -fixed #162
    
    --MAPS--
    -added 3ds test maps.
    -edited and recompiled static prop lighting test maps. concluded test. Source static prop lightmap generation is janky thanks to uv bs. Cant be used for level geometry.
    
    --MDLS & MATS--
    -removed test materials.
    -added test source files for arenaroof model.
    
    -added py script for renaming objects in numerical order. needs object name changed depending on use.
    -identified workflow for creating meshes in blender then exporting as brushes into hammer using wallworm in 3ds max. details in "modelsrc/src/mesh 2 brush instructions.txt"

---
+++ mapsrc/3ds_test8.bat
@@ -0,0 +1,3 @@
+@echo on
+Copy "X:\Programs\Steam\steamapps\common\GarrysMod\garrysmod\addons\Garrys-Chronicles\mapsrc\3ds_test8.bsp" "X:\Programs\Steam\steamapps\common\GarrysMod\garrysmod\maps\3ds_test8.bsp"
+start notepad.exe "X:\Programs\Steam\steamapps\common\GarrysMod\garrysmod\addons\Garrys-Chronicles\mapsrc\3ds_test8_last_compile.log"
